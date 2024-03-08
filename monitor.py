6fd1f769d8b8434fa58984ad0146e0c8d0e3d868
Thato <t.chitja369@gmail.com>
2024-03-08 23:23:39 +0200

    Works with the Task Scheduler

--- monitor.py
+++ monitor.py
@@ -10,7 +10,9 @@ def monitor_performance(interval):
     disk_count = 0
     current_time = datetime.datetime.now()
 
-    with open("ComputerPerformanceLog.txt", "r+") as file:
+    #print("Printing Time of day")
+
+    with open("C:/Users/tchit/OneDrive/effective-fiesta/ComputerPerformanceLog.txt", "r+") as file:
         file.truncate(0)
         if current_time.hour < 12:
             file.write("Good Morning! Here's your morning data\n\n")
@@ -32,13 +34,26 @@ def monitor_performance(interval):
         disk_percent = disk.percent
         disk_count += disk_percent
 
+        #print("Printing results")
+
+        try:
         # Print performance stats to a file
-        with open("ComputerPerformanceLog.txt", "a") as file:
-            file.write(f"Date and Time: {datetime.datetime.now()}\n")
-            file.write(f"CPU usage: {cpu_percent}%\n")
-            file.write(f"Memory Usage: {memory_percent}%\n")
-            file.write(f"Disk Usage: {disk_percent}%\n")
-            file.write("--------------------------------\n")
+            with open("C:/Users/tchit/OneDrive/effective-fiesta/ComputerPerformanceLog.txt", "a") as file:
+                file.write(f"Date and Time: {datetime.datetime.now()}\n")
+                file.write(f"CPU usage: {cpu_percent}%\n")
+                file.write(f"Memory Usage: {memory_percent}%\n")
+                file.write(f"Disk Usage: {disk_percent}%\n")
+                file.write("--------------------------------\n")
+        except FileNotFoundError:
+            print("File not found")
+        except IOError:
+            print("Error reading the file")
+        except Exception as e:
+            print("An error occurred: ", str(e))
+        else:
+            print("File read successfully")
+        finally:
+            print("End of file handling process")
 
         # Wait for the specified interval
         time.sleep(interval)
@@ -51,6 +66,7 @@ def monitor_performance(interval):
     disk_average = "{:.2f}".format(disk_count / loop_count)
 
     # Print performance metrics
+    #print("Printing averages")
     with open("ComputerPerformanceLog.txt", "a") as file:
         file.write(f"CPU Average: {cpu_average}%\n")
         file.write(f"Memory Average: {memory_average}%\n")
